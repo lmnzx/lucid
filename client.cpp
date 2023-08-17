feeb3623578ec42acf90f911950af9b761e3e346
Sayan Mallick <snyxmk@gmail.com>
2023-08-17 21:53:02 +0530

    protocol parsing

--- client.cpp
+++ client.cpp
@@ -9,11 +9,98 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
+}
+
 static void die(const char *msg)
 {
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
+}
+
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = read(fd, buf, n);
+
+        if (rv < 0)
+            return -1; // error or EOF
+
+        assert((ssize_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+
+        if (rv < 0)
+            return -1; // error
+
+        assert((ssize_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t query(int fd, const char *text)
+{
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg)
+        return -1; // too long
+
+    char wbuf[k_max_msg + 4];
+    memcpy(wbuf, &len, 4); // little endian
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len))
+        return err;
+
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+            msg("read() EOF");
+        else
+            msg("read() error");
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4); // little endian
+    if (len > k_max_msg)
+    {
+        msg("message too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+
+    // print reply from server
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+    return 0;
 }
 
 int main()
--- server.cpp
+++ server.cpp
@@ -9,6 +9,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+const size_t k_max_msg = 4096;
+
 static void msg(const char *msg)
 {
     fprintf(stderr, "%s\n", msg);
@@ -21,21 +23,83 @@ static void die(const char *msg)
     abort();
 }
 
-static void do_something(int connfd)
+static int32_t read_full(int fd, char *buf, size_t n)
 {
-    char rbuf[64] = {};
-
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
+    while (n > 0)
     {
-        msg("read() error");
-        return;
+        ssize_t rv = read(fd, buf, n);
+
+        if (rv < 0)
+            return -1; // error or EOF
+
+        assert((ssize_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
 
-    printf("client says: %s\n", rbuf);
+    return 0;
+}
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+
+        if (rv < 0)
+            return -1; // error
+
+        assert((ssize_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t one_request(int connfd)
+{
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+            msg("read() EOF");
+        else
+            msg("read() error");
+        return err;
+    }
+
+    // parse header
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); // little endian
+    if (len > k_max_msg)
+    {
+        msg("message too long");
+        return -1;
+    }
+
+    // read request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+
+    // printing the request body from the client
+    rbuf[4 + len] = '\0'; // null-terminated string
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main()
@@ -74,7 +138,14 @@ int main()
         if (connfd < 0)
             continue; // error is neglected
 
-        do_something(connfd);
+        // only serve one client connection at once
+        while (true)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+                break;
+        }
+
         close(connfd);
     }
 
