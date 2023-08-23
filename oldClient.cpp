08ff17e4cf15f849ef9139bcd10044099e3ea7ae
Sayan Mallick <snyxmk@gmail.com>
2023-08-23 23:27:46 +0530

    added basic commands to the server

--- oldClient.cpp
+++ oldClient.cpp
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <errno.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-
-const size_t k_max_msg = 4096;
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string>
+#include <vector>
 
 static void msg(const char *msg)
 {
@@ -28,15 +28,14 @@ static int32_t read_full(int fd, char *buf, size_t n)
     while (n > 0)
     {
         ssize_t rv = read(fd, buf, n);
-
-        if (rv < 0)
-            return -1; // error or EOF
-
+        if (rv <= 0)
+        {
+            return -1; // error, or unexpected EOF
+        }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
     }
-
     return 0;
 }
 
@@ -45,50 +44,70 @@ static int32_t write_all(int fd, const char *buf, size_t n)
     while (n > 0)
     {
         ssize_t rv = write(fd, buf, n);
-
-        if (rv < 0)
+        if (rv <= 0)
+        {
             return -1; // error
-
+        }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
     }
-
     return 0;
 }
 
-// the `query` function was simply splited into `send_req` and `read_res`.
-static int32_t send_req(int fd, const char *text)
-{
-    uint32_t len = (uint32_t)strlen(text);
-    if (len > k_max_msg)
-        return -1; // too long
+const size_t k_max_msg = 4096;
 
-    char wbuf[k_max_msg + 4];
-    memcpy(wbuf, &len, 4); // little endian
-    memcpy(&wbuf[4], text, len);
+static int32_t send_req(int fd, const std::vector<std::string> &cmd)
+{
+    uint32_t len = 4;
+    for (const std::string &s : cmd)
+    {
+        len += 4 + s.size();
+    }
+    if (len > k_max_msg)
+    {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(&wbuf[0], &len, 4); // assume little endian
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd)
+    {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
     return write_all(fd, wbuf, 4 + len);
 }
 
 static int32_t read_res(int fd)
 {
-    char rbuf[4 + k_max_msg];
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
     errno = 0;
     int32_t err = read_full(fd, rbuf, 4);
     if (err)
     {
         if (errno == 0)
-            msg("read() EOF");
+        {
+            msg("EOF");
+        }
         else
+        {
             msg("read() error");
+        }
         return err;
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf, 4); // little endian
+    memcpy(&len, rbuf, 4); // assume little endian
     if (len > k_max_msg)
     {
-        msg("message too long");
+        msg("too long");
         return -1;
     }
 
@@ -100,47 +119,50 @@ static int32_t read_res(int fd)
         return err;
     }
 
-    // print reply from server
-    rbuf[4 + len] = '\0';
-    printf("server says: %s\n", &rbuf[4]);
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4)
+    {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main()
+int main(int argc, char **argv)
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-
     if (fd < 0)
-        die("socket");
+    {
+        die("socket()");
+    }
 
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1
-
-    // connect to server
-    int rv = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
-
-    if (rv < 0)
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv)
+    {
         die("connect");
-
-    // multiple pipelined requests
-    const char *query_list[3] = {"hello1", "hello2", "hello3"};
-    for (size_t i = 0; i < 3; ++i)
-    {
-        int32_t err = send_req(fd, query_list[i]);
-        if (err)
-        {
-            goto L_DONE;
-        }
     }
-    for (size_t i = 0; i < 3; ++i)
+
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i)
     {
-        int32_t err = read_res(fd);
-        if (err)
-        {
-            goto L_DONE;
-        }
+        cmd.push_back(argv[i]);
+    }
+    int32_t err = send_req(fd, cmd);
+    if (err)
+    {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err)
+    {
+        goto L_DONE;
     }
 
 L_DONE:
